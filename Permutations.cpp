da11c48f7228631c23e665fb7aa678259618e1cb
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-05-12 13:22:18 +0530

    Update Permutations.cpp

--- Permutations.cpp
+++ Permutations.cpp
@@ -3,12 +3,23 @@
 
 using namespace std;
 
+/*
+ Intuition: Start at n and assign even numbers to the first half
+ serially and odd numbers to the second half serially
+ if n is odd then the second half starts at n/2+1
+ else n is even the the second half start at n/2
+
+ Time-complexity: O(n) where n is the given number
+ Space-complexity: O(1) apart from resultant array
+*/
+
 vector<int> perminator(vector<int> &given, int n)
 {
     vector<int> res(n, -1);
-    int oddStrt=0, evenStrt = n%2==0? n/2: n/2+1;
-    for(int i=n; i>=1; i--){
-        if(i%2==0)
+    int oddStrt = 0, evenStrt = n % 2 == 0 ? n / 2 : n / 2 + 1;
+    for (int i = n; i >= 1; i--)
+    {
+        if (i % 2 == 0)
             res[evenStrt++] = i;
         else
             res[oddStrt++] = i;
@@ -20,9 +31,12 @@ int main()
 {
     int n, a;
     cin >> n;
-    if(n==1) cout<<1<<endl;
-    else if(n<=3) cout<<"NO SOLUTION"<<endl;
-    else{
+    if (n == 1)
+        cout << 1 << endl;
+    else if (n <= 3)
+        cout << "NO SOLUTION" << endl;
+    else
+    {
         vector<int> given(n);
         int i = 0;
         while (cin >> a)
@@ -30,8 +44,9 @@ int main()
             given[i++] = a;
         }
         vector<int> ans = perminator(given, n);
-        for(int a: ans){
-            cout<<a<<" ";
+        for (int a : ans)
+        {
+            cout << a << " ";
         }
     }
     return 0;
